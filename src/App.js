baf6fe282f3c1b17cb91583ab653998c6fde1a50
zk <liuzq7@gmail.com>
2022-08-21 19:34:54 +0800

    chore: add antd

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React from "react";
-import TodoForm from './TodoForm'
-import Todo from './Todo'
-import useTodo from './useTodo'
-import "./App.css";
-
+import Dirt from './Dirt'
 
 function App() {
-  const [todos,addTodo,completeTodo,deleteTodo] =  useTodo()
-  // useEffect(() => {
-  //   console.log("hello,useEffect");
-  //   return () => {
-  //     console.log("out,useEffect");
-  //   };
-  // });
   return (
     <div className="app">
-      <div className="todo-list">
-        {todos.map((todo, index) => {
-          return (
-            <Todo
-              key={index}
-              index={index}
-              todo={todo}
-              completeTodo={completeTodo}
-              deleteTodo={deleteTodo}
-            />
-          );
-        })}
-        <TodoForm addTodo={addTodo} />
-      </div>
+      <Dirt/>
     </div>
   );
 }
---
+++ src/Dirt.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { DatePicker } from 'antd';
+
+function Dirt(props) {
+  return (
+    <>
+      <DatePicker />
+    </>
+  );
+}
+
+export default Dirt;
