031edb1f96586d7d28988923952dd8f8bbf8ccfd
zk <liuzq7@gmail.com>
2022-08-21 23:36:24 +0800

    chore: dynamic header

--- src/Dirt.js
+++ src/Dirt.js
@@ -1,88 +1,100 @@
 import { Switch, Table } from 'antd';
-import React, { useState } from 'react';
-const columns = [
-  {
-    title: 'Full Name',
-    width: 100,
-    dataIndex: 'name',
-    key: 'name',
-    fixed: 'left',
-  },
-  {
-    title: 'Age',
-    width: 100,
-    dataIndex: 'age',
-    key: 'age',
-    fixed: 'left',
-  },
-  {
-    title: 'Column 1',
-    dataIndex: 'address',
-    key: '1',
-    width: 150,
-  },
-  {
-    title: 'Column 2',
-    dataIndex: 'address',
-    key: '2',
-    width: 150,
-  },
-  {
-    title: 'Column 3',
-    dataIndex: 'address',
-    key: '3',
-    width: 150,
-  },
-  {
-    title: 'Column 4',
-    dataIndex: 'address',
-    key: '4',
-    width: 150,
-  },
-  {
-    title: 'Column 5',
-    dataIndex: 'address',
-    key: '5',
-    width: 150,
-  },
-  {
-    title: 'Column 6',
-    dataIndex: 'address',
-    key: '6',
-    width: 150,
-  },
-  {
-    title: 'Column 7',
-    dataIndex: 'address',
-    key: '7',
-    width: 150,
-  },
-  {
-    title: 'Column 8',
-    dataIndex: 'address',
-    key: '8',
-  },
-  {
-    title: 'Action',
-    key: 'operation',
-    fixed: 'right',
-    width: 100,
-    render: () => <a>action</a>,
-  },
-];
-const data = [];
+import axios from 'axios';
+import React, { useState,useEffect } from 'react';
+// const columns = [
+//   {
+//     title: 'Full Name',
+//     width: 100,
+//     dataIndex: 'name',
+//     key: 'name',
+//     fixed: 'left',
+//   },
+//   {
+//     title: 'Age',
+//     width: 100,
+//     dataIndex: 'age',
+//     key: 'age',
+//     fixed: 'left',
+//   },
+//   {
+//     title: 'Column 1',
+//     dataIndex: 'address',
+//     key: '1',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 2',
+//     dataIndex: 'address',
+//     key: '2',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 3',
+//     dataIndex: 'address',
+//     key: '3',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 4',
+//     dataIndex: 'address',
+//     key: '4',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 5',
+//     dataIndex: 'address',
+//     key: '5',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 6',
+//     dataIndex: 'address',
+//     key: '6',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 7',
+//     dataIndex: 'address',
+//     key: '7',
+//     width: 150,
+//   },
+//   {
+//     title: 'Column 8',
+//     dataIndex: 'address',
+//     key: '8',
+//   },
+//   {
+//     title: 'Action',
+//     key: 'operation',
+//     fixed: 'right',
+//     width: 100,
+//     render: () => <a>action</a>,
+//   },
+// ];
+  const data = [];
 
-for (let i = 0; i < 100; i++) {
-  data.push({
-    key: i,
-    name: `Edrward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: `Edrward ${i}`,
+      age: 32,
+      address: `London Park no. ${i}`,
+    });
+  }
 
 const Dirt = () => {
   const [fixedTop, setFixedTop] = useState(false);
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+
+    axios.get('http://127.0.0.1:8081/getTableHeaders?pageId=dirt')
+      .then(res => {
+        if (res.data.code == 0)
+          setColumns(res.data.data)
+      })
+
+  })
   return (
     <Table
       columns={columns}
