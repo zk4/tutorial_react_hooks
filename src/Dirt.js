8704851e841ce2ff8036c0a389e830dc63009982
zk <liuzq7@gmail.com>
2022-08-23 21:27:15 +0800

    bug: so many

--- src/Dirt.js
+++ src/Dirt.js
@@ -23,125 +23,125 @@ import request from 'umi-request';
 //   closed_at?: string;
 // };
 
-const columns /*: ProColumns<GithubIssueItem>[]*/ = [
-  {
-    dataIndex: 'index',
-    valueType: 'indexBorder',
-    width: 48,
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-    copyable: true,
-    ellipsis: true,
-    tip: '标题过长会自动收缩',
-    formItemProps: {
-      rules: [
-        {
-          required: true,
-          message: '此项为必填项',
-        },
-      ],
-    },
-  },
-  {
-    disable: true,
-    title: '状态',
-    dataIndex: 'state',
-    filters: true,
-    onFilter: true,
-    ellipsis: true,
-    valueType: 'select',
-    valueEnum: {
-      all: {text: '超长'.repeat(50)},
-      open: {
-        text: '未解决',
-        status: 'Error',
-      },
-      closed: {
-        text: '已解决',
-        status: 'Success',
-        disabled: true,
-      },
-      processing: {
-        text: '解决中',
-        status: 'Processing',
-      },
-    },
-  },
-  {
-    disable: true,
-    title: '标签',
-    dataIndex: 'labels',
-    search: false,
-    renderFormItem: (_, {defaultRender}) => {
-      return defaultRender(_);
-    },
-    render: (_, record) => (
-      <Space>
-        {record.labels.map(({name, color}) => (
-          <Tag color={color} key={name}>
-            {name}
-          </Tag>
-        ))}
-      </Space>
-    ),
-  },
-  {
-    title: '创建时间',
-    key: 'showTime',
-    dataIndex: 'created_at',
-    valueType: 'dateTime',
-    sorter: true,
-    hideInSearch: true,
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'created_at',
-    valueType: 'dateRange',
-    hideInTable: true,
-    search: {
-      transform: (value) => {
-        return {
-          startTime: value[0],
-          endTime: value[1],
-        };
-      },
-    },
-  },
-  {
-    title: '操作',
-    valueType: 'option',
-    key: 'option',
-    render: (text, record, _, action) => [
-      <a
-        key="editable"
-        onClick={() => {
-            if (action) {
-              let editable = action.startEditable;
-              if (editable) {
-                editable(record.id);
-              }
-            }
-          }
-        }
-      >
-        编辑
-      </a>,
-      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-        查看
-      </a>,
-      <TableDropdown
-        key="actionGroup"
-        onSelect={() =>  action.reload()}
-        menus={[
-          {key: 'copy', name: '复制'},
-          {key: 'delete', name: '删除'},
-        ]}
-      />,
-    ],
-  },
-];
+// const columns [>: ProColumns<GithubIssueItem>[]<] = [
+//   {
+//     dataIndex: 'index',
+//     valueType: 'indexBorder',
+//     width: 48,
+//   },
+//   {
+//     title: '标题',
+//     dataIndex: 'title',
+//     copyable: true,
+//     ellipsis: true,
+//     tip: '标题过长会自动收缩',
+//     formItemProps: {
+//       rules: [
+//         {
+//           required: true,
+//           message: '此项为必填项',
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     disable: true,
+//     title: '状态',
+//     dataIndex: 'state',
+//     filters: true,
+//     onFilter: true,
+//     ellipsis: true,
+//     valueType: 'select',
+//     valueEnum: {
+//       all: {text: '超长'.repeat(50)},
+//       open: {
+//         text: '未解决',
+//         status: 'Error',
+//       },
+//       closed: {
+//         text: '已解决',
+//         status: 'Success',
+//         disabled: true,
+//       },
+//       processing: {
+//         text: '解决中',
+//         status: 'Processing',
+//       },
+//     },
+//   },
+//   {
+//     disable: true,
+//     title: '标签',
+//     dataIndex: 'labels',
+//     search: false,
+//     renderFormItem: (_, {defaultRender}) => {
+//       return defaultRender(_);
+//     },
+//     render: (_, record) => (
+//       <Space>
+//         {record.labels.map(({name, color}) => (
+//           <Tag color={color} key={name}>
+//             {name}
+//           </Tag>
+//         ))}
+//       </Space>
+//     ),
+//   },
+//   {
+//     title: '创建时间',
+//     key: 'showTime',
+//     dataIndex: 'created_at',
+//     valueType: 'dateTime',
+//     sorter: true,
+//     hideInSearch: true,
+//   },
+//   {
+//     title: '创建时间',
+//     dataIndex: 'created_at',
+//     valueType: 'dateRange',
+//     hideInTable: true,
+//     search: {
+//       transform: (value) => {
+//         return {
+//           startTime: value[0],
+//           endTime: value[1],
+//         };
+//       },
+//     },
+//   },
+//   {
+//     title: '操作',
+//     valueType: 'option',
+//     key: 'option',
+//     render: (text, record, _, action) => [
+//       <a
+//         key="editable"
+//         onClick={() => {
+//             if (action) {
+//               let editable = action.startEditable;
+//               if (editable) {
+//                 editable(record.id);
+//               }
+//             }
+//           }
+//         }
+//       >
+//         编辑
+//       </a>,
+//       <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+//         查看
+//       </a>,
+//       <TableDropdown
+//         key="actionGroup"
+//         onSelect={() =>  action.reload()}
+//         menus={[
+//           {key: 'copy', name: '复制'},
+//           {key: 'delete', name: '删除'},
+//         ]}
+//       />,
+//     ],
+//   },
+// ];
 
 const menu = (
   <Menu
@@ -163,6 +163,17 @@ const menu = (
 );
 
 export default function Dirt() {
+  const [columns, setColumns] = useState([]);
+  useEffect(()=>{
+      axios.get('http://127.0.0.1:8081/getTableHeaders?tableName=GithubIssue')
+        .then(res => {
+          if (res.data.code == 0)
+          {
+            console.log(res.data)
+            setColumns(res.data.data)
+          }
+        })
+  },[])
   const actionRef = useRef();
   return (
     <ProTable
@@ -170,8 +181,31 @@ export default function Dirt() {
       actionRef={actionRef}
       cardBordered
       request={async (params = {}, sort, filter) => {
-        console.log(sort, filter);
-        return request('https://proapi.azurewebsites.net/github/issues', {
+        const tableName = 'Container';
+
+        // 映射 current 到 pageNumber
+        params.pageNumber = params.current;
+        delete params["current"];
+
+        // 制作 JPA filters   
+        let paramsCpy = Object.assign({},params)
+        delete paramsCpy["pageNumber"];
+        delete paramsCpy["pageSize"];
+
+
+    const filters =  Object.entries(paramsCpy)
+    .filter(([key,value])=>{return value!=null;})
+    .map(([key,value])=>{ return key+"=="+value;})
+    .join(";");
+
+        console.log("paramsCpy",paramsCpy,"params",params,"sort",sort, "filters:",filters);
+        // 只能用字符串拼，不然会转义
+        // let myParams = "filter=url==%25http%25;number==1";
+        let myParams = `filter=${filters}`;
+
+        let url = `http://127.0.0.1:8081/getDatas?tableName=${tableName}&${myParams}`;
+        console.log("url",url);
+        return request(url, {
           params,
         });
       }}
@@ -200,7 +234,8 @@ export default function Dirt() {
           if (type === 'get') {
             return {
               ...values,
-              created_at: [values.startTime, values.endTime],
+              // 注了
+              // created_at: [values.startTime, values.endTime],
             };
           }
           return values;
