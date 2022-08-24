1f88ca21ee01ab0c6187001d978b6d144e47f029
zk <liuzq7@gmail.com>
2022-08-24 20:01:53 +0800

    chore: clean up

--- src/Dirt.js
+++ src/Dirt.js
@@ -1,164 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
-import {
-  EllipsisOutlined, PlusOutlined,
-
-} from '@ant-design/icons';
-import {
-  ProTable,
-  ModalForm,
-  ProForm,
-  ProFormDateRangePicker,
-  ProFormSelect,
-  ProFormText,
-} from '@ant-design/pro-components';
-import {Button, Dropdown, Menu, message} from 'antd';
-import {useRef} from 'react';
+import {PlusOutlined, } from '@ant-design/icons';
+import {ProTable, ModalForm, ProForm, ProFormText, } from '@ant-design/pro-components';
+import {Button, message} from 'antd';
 import request from 'umi-request';
 
-const waitTime = (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, time);
-  });
-};
-
-// type GithubIssueItem = {
-//   url: string;
-//   id: number;
-//   number: number;
-//   title: string;
-//   labels: {
-//     name: string;
-//     color: string;
-//   }[];
-//   state: string;
-//   comments: number;
-//   created_at: string;
-//   updated_at: string;
-//   closed_at?: string;
-// };
-
-// const columns [>: ProColumns<GithubIssueItem>[]<] = [
-//   {
-//     dataIndex: 'index',
-//     valueType: 'indexBorder',
-//     width: 48,
-//   },
-//   {
-//     title: '标题',
-//     dataIndex: 'title',
-//     copyable: true,
-//     ellipsis: true,
-//     tip: '标题过长会自动收缩',
-//     formItemProps: {
-//       rules: [
-//         {
-//           required: true,
-//           message: '此项为必填项',
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     disable: true,
-//     title: '状态',
-//     dataIndex: 'state',
-//     filters: true,
-//     onFilter: true,
-//     ellipsis: true,
-//     valueType: 'select',
-//     valueEnum: {
-//       all: {text: '超长'.repeat(50)},
-//       open: {
-//         text: '未解决',
-//         status: 'Error',
-//       },
-//       closed: {
-//         text: '已解决',
-//         status: 'Success',
-//         disabled: true,
-//       },
-//       processing: {
-//         text: '解决中',
-//         status: 'Processing',
-//       },
-//     },
-//   },
-//   {
-//     disable: true,
-//     title: '标签',
-//     dataIndex: 'labels',
-//     search: false,
-//     renderFormItem: (_, {defaultRender}) => {
-//       return defaultRender(_);
-//     },
-//     render: (_, record) => (
-//       <Space>
-//         {record.labels.map(({name, color}) => (
-//           <Tag color={color} key={name}>
-//             {name}
-//           </Tag>
-//         ))}
-//       </Space>
-//     ),
-//   },
-//   {
-//     title: '创建时间',
-//     key: 'showTime',
-//     dataIndex: 'created_at',
-//     valueType: 'dateTime',
-//     sorter: true,
-//     hideInSearch: true,
-//   },
-//   {
-//     title: '创建时间',
-//     dataIndex: 'created_at',
-//     valueType: 'dateRange',
-//     hideInTable: true,
-//     search: {
-//       transform: (value) => {
-//         return {
-//           startTime: value[0],
-//           endTime: value[1],
-//         };
-//       },
-//     },
-//   },
-//   {
-//     title: '操作',
-//     valueType: 'option',
-//     key: 'option',
-//     render: (text, record, _, action) => [
-//       <a
-//         key="editable"
-//         onClick={() => {
-//             if (action) {
-//               let editable = action.startEditable;
-//               if (editable) {
-//                 editable(record.id);
-//               }
-//             }
-//           }
-//         }
-//       >
-//         编辑
-//       </a>,
-//       <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-//         查看
-//       </a>,
-//       <TableDropdown
-//         key="actionGroup"
-//         onSelect={() =>  action.reload()}
-//         menus={[
-//           {key: 'copy', name: '复制'},
-//           {key: 'delete', name: '删除'},
-//         ]}
-//       />,
-//     ],
-//   },
-// ];
 
 export default function Dirt(props) {
   const tableName = props.tableName;
@@ -205,7 +51,6 @@ export default function Dirt(props) {
     )
     if (res.data.code === 0) {
       if (res.data) {
-        // setColumns(res.data.data)
         message.success('提交成功');
         search();
       }
@@ -215,6 +60,15 @@ export default function Dirt(props) {
   }
 
   const generateForm = () => {
+    let grouped = columns
+      .filter(c => c.submitable)
+      .reduce((a, c) => {
+        if (!a[c.submitFormGroupId]) a[c.submitFormGroupId] = []
+        a[c.submitFormGroupId].push(c)
+        return a;
+      }, {})
+    console.log("grouped", grouped)
+
     return <ModalForm title="创建" trigger={
       <Button type="primary">
         <PlusOutlined /> 创建
@@ -226,19 +80,24 @@ export default function Dirt(props) {
       onFinish={onCreate}
     >
       {
-        columns
-          .filter(c => c.submitable)
-          .map(c => {
-            return <ProForm.Group >
-              <ProFormText
-                width="md"
-                name={c.key}
-                label={c.submitLabel}
-                tooltip={c.submitTooltip}
-                placeholder={c.submitPlaceholder}
-              />
-            </ProForm.Group>
-          })}
+        Object.entries(grouped).map(([i, cs]) => {
+          return <ProForm.Group >
+            {
+              cs.map(c =>
+                <ProFormText
+                  width={c.submitWidth}
+                  name={c.key}
+                  label={c.submitLabel}
+                  tooltip={c.submitTooltip}
+                  placeholder={c.submitPlaceholder}
+                />
+
+              )
+            }
+          </ProForm.Group>
+
+        })
+      }
     </ModalForm>
   }
   return (
