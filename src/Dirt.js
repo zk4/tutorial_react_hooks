6a0246fedf68980921fa54f2fba1b84d8c8388cf
zk <liuzq7@gmail.com>
2022-08-24 17:26:36 +0800

    chore: u

--- src/Dirt.js
+++ src/Dirt.js
@@ -163,7 +163,7 @@ const waitTime = (time) => {
 export default function Dirt(props) {
   const tableName = props.tableName;
   const [columns, setColumns] = useState([]);
-  useEffect(() => {
+  const refreshHeader = () => {
     axios.get(`http://127.0.0.1:8081/getTableHeaders?tableName=${tableName}`)
       .then(res => {
         if (res.data.code === 0) {
@@ -171,79 +171,11 @@ export default function Dirt(props) {
           setColumns(res.data.data)
         }
       })
-  }, [])
+  }
+  useEffect(() => {refreshHeader();}, [])
   const actionRef = useRef();
 
-
-  const  generateForm = ()=>{
-    return <ModalForm title="创建" trigger={ 
-    <Button type="primary"> 
-        <PlusOutlined /> 创建
-            </Button>
-          }
-          autoFocusFirstInput
-          modalProps={{
-            onCancel: () => console.log('run'),
-          }}
-          submitTimeout={2000}
-          onFinish={async (values) => {
-            await waitTime(2000);
-            console.log(values.name);
-            message.success('提交成功');
-            return true;
-          }}
-        >
-          <ProForm.Group>
-            <ProFormText
-              width="md"
-              name="name"
-              label="签约客户名称"
-              tooltip="最长为 24 位"
-              placeholder="请输入名称"
-            />
-
-            <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
-          </ProForm.Group>
-          <ProForm.Group>
-            <ProFormText width="md" name="contract" label="合同名称" placeholder="请输入名称" />
-            <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
-          </ProForm.Group>
-          <ProForm.Group>
-            <ProFormSelect
-              request={async () => [
-                {
-                  value: 'chapter',
-                  label: '盖章后生效',
-                },
-              ]}
-              width="xs"
-              name="useMode"
-              label="合同约定生效方式"
-            />
-            <ProFormSelect
-              width="xs"
-              options={[
-                {
-                  value: 'time',
-                  label: '履行完终止',
-                },
-              ]}
-              name="unusedMode"
-              label="合同约定失效效方式"
-            />
-          </ProForm.Group>
-          <ProFormText width="sm" name="id" label="主合同编号" />
-          <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
-          <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
-        </ModalForm>
-  }
-  return (
-    <ProTable
-      columns={columns}
-      actionRef={actionRef}
-      cardBordered
-      request={async (params = {}, sort, filter) => {
-
+  const search = async (params = {}, sort, filter) => {
         // 映射 current 到 pageNumber
         params.pageNumber = params.current;
         delete params["current"];
@@ -264,7 +196,59 @@ export default function Dirt(props) {
 
         let url = `http://127.0.0.1:8081/getDatas?tableName=${tableName}&${myParams}`;
         return request(url, {params, });
+      }
+
+  const generateForm = () => {
+    return <ModalForm title="创建" trigger={
+      <Button type="primary">
+        <PlusOutlined /> 创建
+      </Button>
+    }
+      autoFocusFirstInput
+      modalProps={{
+        onCancel: () => console.log('run'),
       }}
+      submitTimeout={4000}
+
+      onFinish={async (values) => {
+        debugger
+        let res = await axios.post(`http://127.0.0.1:8081/dirt/create?tableName=${tableName}`, {
+          ...values
+        }
+        )
+        if (res.data.code === 0) {
+          if (res.data) {
+            // setColumns(res.data.data)
+            message.success('提交成功');
+            search();
+          }
+        } else {
+        }
+        return true;
+      }}
+    >
+      {
+        columns.map(c => {
+          return <ProForm.Group >
+            <ProFormText
+              width="md"
+              name={c.key}
+              label={c.submitLabel}
+              tooltip={c.submitTooltip}
+              placeholder={c.submitPlaceholder}
+            />
+
+          </ProForm.Group>
+
+        })}
+    </ModalForm>
+  }
+  return (
+    <ProTable
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      request={search}
       editable={{
         type: 'multiple',
       }}
