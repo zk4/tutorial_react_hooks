ef751f60eac72b1985c0ef11ed1850dce94a739e
zk <liuzq7@gmail.com>
2022-08-25 14:43:48 +0800

    chore: format

--- src/Dirt.js
+++ src/Dirt.js
@@ -4,25 +4,10 @@ import React, {
   useRef
 } from 'react';
 import axios from 'axios';
-import {
-  PlusOutlined,
-} from '@ant-design/icons';
-import {
-  ProTable,
-  ModalForm,
-  ProForm,
-  ProFormText,
-} from '@ant-design/pro-components';
-import {
-  Button,
-  message
-} from 'antd';
+import {PlusOutlined, } from '@ant-design/icons';
+import {ProTable, BetaSchemaForm, } from '@ant-design/pro-components';
+import {Button, message} from 'antd';
 import request from 'umi-request';
-import DirtSchemeForm from './DirtSchemeForm'
-import {
-  BetaSchemaForm,
-  ProFormSelect
-} from '@ant-design/pro-components';
 
 
 export default function Dirt(props) {
@@ -75,7 +60,7 @@ export default function Dirt(props) {
   }
 
   const onCreate = async (values) => {
-    console.log("onCreate",values)
+    console.log("onCreate", values)
     try {
       let res = await axios.post(`http://127.0.0.1:8081/dirt/create?tableName=${tableName}`, {
         ...values
@@ -110,88 +95,84 @@ export default function Dirt(props) {
       title="创建"
       trigger={
         <Button type="primary">
-          <PlusOutlined /> 
-          创建 
-        </Button> 
-        }
-        layoutType = 'ModalForm'
-        columns = { submitTypes }
-        autoFocusFirstInput
-        modalProps = { { onCancel: () => console.log('run'), } }
-        submitTimeout = { 4000 }
-        rowProps = { { gutter: [16, 16], } }
-        colProps = { { span: 12, } }
-        grid = { true }
-        onFinish = { v=>onCreate(v) } >
+          <PlusOutlined />
+          创建
+        </Button>
+      }
+      layoutType='ModalForm'
+      columns={submitTypes}
+      autoFocusFirstInput
+      modalProps={{onCancel: () => console.log('run'), }}
+      submitTimeout={4000}
+      rowProps={{gutter: [16, 16], }}
+      colProps={{span: 12, }}
+      grid={true}
+      onFinish={v => onCreate(v)} >
 
-        </BetaSchemaForm>
-	}
-          return ( <
-            ProTable columns={
-              columns
-            }
-            actionRef={
-              actionRef
-            }
-            cardBordered request={
-              searchPromise
-            }
-            editable={
-              {
-                type: 'multiple',
-              }
-            }
-            columnsState={
-              {
-                persistenceKey: 'pro-table-singe-demos',
-                persistenceType: 'localStorage',
-                onChange(value) {
-                  console.log('value: ', value);
-                },
-              }
-            }
-            rowKey="id"
-            search={
-              {
-                labelWidth: 'auto',
-              }
-            }
-            options={
-              {
-                setting: {
-                  listsHeight: 400,
-                },
-              }
-            }
-            form={
-              {
-                // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
-                syncToUrl: (values, type) => {
-                  if (type === 'get') {
-                    return {
-                      ...values,
-                      // closed_at:new Date(values.closed_at).toISOString()
-                      // 注了
-                      // created_at: [values.startTime, values.endTime],
-                    };
-                  }
-                  return values;
-                },
-              }
-            }
-            pagination={
-              {
-                pageSize: 5,
-                onChange: (page) => console.log(page),
-              }
-            }
-            dateFormatter="string"
-            headerTitle=""
-            toolBarRender={
-              () => [
-                generateForm()
-              ]
-            }
-          />
-          );
+    </BetaSchemaForm>
+  }
+  return (<ProTable
+    columns={columns}
+    actionRef={actionRef}
+    cardBordered request={
+      searchPromise
+    }
+    editable={
+      {
+        type: 'multiple',
+      }
+    }
+    columnsState={
+      {
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }
+    }
+    rowKey="id"
+    search={
+      {
+        labelWidth: 'auto',
+      }
+    }
+    options={
+      {
+        setting: {
+          listsHeight: 400,
+        },
+      }
+    }
+    form={
+      {
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              // closed_at:new Date(values.closed_at).toISOString()
+              // 注了
+              // created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }
+    }
+    pagination={
+      {
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }
+    }
+    dateFormatter="string"
+    headerTitle=""
+    toolBarRender={
+      () => [
+        generateForm()
+      ]
+    }
+  />
+  );
 };
